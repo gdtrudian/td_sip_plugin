328bc7265e4c209d6657f9a3bbcc271def923c79
trudian <github@farbell.net>
2023-04-22 11:32:02 +0800

    0.0.9

--- test/td_sip_plugin_test.dart
+++ test/td_sip_plugin_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:td_sip_plugin/td_sip_plugin.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('td_sip_plugin');
